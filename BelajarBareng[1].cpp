2a0e7fcde4e8ee3b2b30a723d3dea8075795755e
HaniefuL <haniefulgeofarry@gmail.com>
2024-06-09 21:56:02 +0700

    first commit

---
+++ BelajarBareng[1].cpp
@@ -0,0 +1,11 @@
+/*  
+
+    Buatlah program untuk perhitungan matematika +, -, * dan /
+    Dengan base class Perhitungan yang memiliki method input, perhitungan dan display.
+    class diatas diturunkan ke class Tambah, Kurang, Kali dan Bagi.
+    Buatlah program bisa diulang jika user ingin mengulainya.
+    Gunakan paradigma OOP untuk mengerjakan. Abstraksi, Inheritance, Encapsulation, Polymorphy. 
+
+*/
+
+
